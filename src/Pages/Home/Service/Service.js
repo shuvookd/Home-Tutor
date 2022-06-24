dd6ec0d29a876eb5de2887cc2a5333655b53b113
shuvokd <shuvokd1210@gmail.com>
2022-06-24 20:25:10 +0600

    Service added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Service.css';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {id, name, img, description, price} = service;
+    const navigate = useNavigate();
+
+    const navigateToServiceDetail = id =>{
+        navigate(`/service/${id}`);
+    }
     return (
-        <div>
-            
+        <div className='service'>
+            <img className='w-100' src={img} alt="" />
+            <h2>{name}</h2>
+            <p>Price: {price}</p>
+            <p><small>{description}</small></p>
+            <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
         </div>
     );
 };
